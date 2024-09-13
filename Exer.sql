683c37c1b7dba610837aa3e4871e5409f211fcb6
João V. Nunes <127100474+JVNunes50@users.noreply.github.com>
2024-09-13 20:11:33 -0300

    Create Exer.sql

---
+++ Exer.sql
@@ -0,0 +1,54 @@
+pag_pagto = ISNULL (@data, getdate()),
+fkbanco = @fkbanco
+WHERE idpagar = @idpagar
+
+
+CREATE TABLE movbanco(
+	IdMovbanco INT IDENTITY(1,1) NOT NULL PRIMARY KEY, --PK
+	Movban_Descricao VARCHAR (30) NULL,
+	Movban_Saldo NUMERIC (18, 2) NULL,
+	Movban_date DATETIME,
+	Movban_valor NUMERIC (18,2),
+	Movban_tipo INT,
+	fkbanco INT
+)
+
+ALTER TABLE movbanco
+ADD CONSTRAINT fkbanco
+FOREIGN KEY (fkbanco)
+REFERENCES IdMovbanco (IdMovbanco);
+
+INSERT movbanco(
+	Movban_descricao,
+	Movban_date,
+	Movban_valor,
+	Movban_tipo,
+	Movban_saldo,
+	fkbanco 
+)
+
+VALUES
+
+('pagto fatura' + ISNULL(@fatura,)'**') ISNULL (@data, getdate())
+
+DROP PROC sp_i_pagamento
+go
+CREATE PROC sp_I_pagamento (
+	@idpagar int,
+	@fatura VARCHAR(15),
+	@fkbanco INT,
+	@valor NUMERIC(18,2),
+	@data DATE
+)
+
+AS
+
+	DECLARE @saldo NUMERIC(18,2)
+	SET @saldo = (SELECT Ban_saldo FROM banco WHERE IdBanco = @fkbanco)
+
+	-- testa se a fatura existe
+	IF NOT EXISTS (SELECT Ban_saldo FROM banco WHERE IdBanco = @fkbanco)
+	BEGIN
+		SELECT 'A fatura fornecida não existe!'
+		return -1
+	END
